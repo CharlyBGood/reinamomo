3e8b92d778fbcc7d31a63db28721d4e0393d6685
charlybgood <bonavita.carlos@gmail.com>
2021-12-03 02:37:58 -0300

    trying animations

---
+++ main.js
@@ -0,0 +1,16 @@
+let header = document.getElementById('header');
+
+let loader = document.getElementById('loader');
+
+let logoButton = document.getElementById('logo_button');
+logoButton.addEventListener('click', changeBckg);
+ 
+
+
+function changeBckg() {
+    // header.style.opacity = .3;
+    
+    
+    console.log('hello! Queen');
+}
+
