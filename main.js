fcb56ae07b27deadb906dff501d99cee3b74ccc7
charlybgood <bonavita.carlos@gmail.com>
2021-12-04 00:50:47 -0300

    testing

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ let header = document.getElementById('header');
 
 let loader = document.getElementById('loader');
 
+let body = document.getElementById('body');
+let title = document.getElementById('title');
+
 let logoButton = document.getElementById('logo_button');
 logoButton.addEventListener('click', changeBckg);
  
@@ -9,8 +12,8 @@ logoButton.addEventListener('click', changeBckg);
 
 function changeBckg() {
     // header.style.opacity = .3;
-    
-    
+    body.style.backgroundColor = '#000';
+    title.style.color = '#d1e2c0'
     console.log('hello! Queen');
 }
 
